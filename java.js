2f7ea9a1b6d6c7fbd68751805464e507772c36d5
alinakanwal <umartararumartarar8@gmail.com>
2024-08-08 15:35:49 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,44 @@
+document.getElementById('formvalidation').addEventListener('submit',function(event){
+ document.getElementById('errorusername').textContent = '';
+ document.getElementById('emailerror').textContent = '';
+ document.getElementById('errorpassword').textContent = '';
+ document.getElementById('confirmPasswordError').textContent = '';
+let isvalid = true;
+
+// access input value
+
+let username = document.getElementById('username').value.trim();
+let email = document.getElementById('email').value.trim();
+let password = document.getElementById('Password').value;
+let confirm = document.getElementById('confirmPassword').value;
+
+// condition
+
+if(username === ""){
+    document.getElementById('errorusername').textContent = "Username is required."
+    isvalid = false;
+}
+
+let emailpattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if(email === "" || !emailpattern.test(email)){
+    document.getElementById('emailerror').textContent = "Please enter valid email."
+    isvalid = false;
+}
+
+if(password.length < 6){
+    document.getElementById('errorpassword').textContent = 'Password must be at least 6 characters long.'
+isvalid = false;
+}
+
+if (password !== confirm) {
+    document.getElementById('confirmPasswordError').textContent = 'Passwords do not match.';
+    isvalid = false; 
+}
+
+
+
+if(!isvalid){
+    event.preventDefault();
+}
+
+})
